f3abc0c7ff70a7dc4f7b466395593dbcded4b318
Michael <michael.miles2468@gmail.com>
2022-07-12 17:40:02 -0400

    Intgrated user geolocation, integrated accuwearther API for users local forcasts. Seemingly running well but randomly thrown a CORS error

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,44 @@ import UnauthenticatedApp from './UnauthenticateApp';
 function App() {
   const [currentUser, setCurrentUser] = useState(false)
   const [authCheck, setAuthCheck] = useState(false)
+  const [userLocationKey, setUserLocationKey] = useState(null)
+  const [localTemps, setLocalTemps] = useState({})
+  const [lat, setLat] = useState(null)
+  const [lng, setLng] = useState(null)
+  const [status, setStatus] = useState(null)
+
+  function getLocation () {
+    if (!navigator.geolocation) {
+      setStatus('Geolocation not supported by your browser.');
+    } else {
+      setStatus('Locating...');
+      navigator.geolocation.getCurrentPosition((position) => {
+        setStatus(null);
+        setLat(position.coords.latitude);
+        setLng(position.coords.longitude);
+      }, () => {
+        setStatus('Unable to retrive your location.');
+      })
+    }
+  }
+
+  useEffect(() => {
+    fetch('http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=OsURg1PXKrkIswSEQHCGvY7yHyTJpVkP&q=40.2273547%2C-74.0025902')
+    .then((res) => res.json())
+    .then((res) => {
+      setUserLocationKey(res.Key)
+      return fetch(`http://dataservice.accuweather.com/forecasts/v1/daily/1day/${res.Key}?apikey=OsURg1PXKrkIswSEQHCGvY7yHyTJpVkP`)
+      })
+      .then(res => res.json())
+      .then(res => {
+        console.log(res.DailyForecasts.Temperature)
+        setLocalTemps(res.DailyForecasts.Temperature)
+      })
+    })
+    // .then(fetch(`http://dataservice.accuweather.com/forecasts/v1/daily/1day/${userLocationKey}?apikey=OsURg1PXKrkIswSEQHCGvY7yHyTJpVkP`)
+    // .then(res => res.json)
+    // .then(res => console.log(res))) 
+  // })
 
   return (
     <BrowserRouter>
--- client/src/components/HomePageBanner.js
+++ client/src/components/HomePageBanner.js
@@ -1,10 +1,37 @@
+import {useState, useEffect} from 'react'
+
 function HomePageBanner () {
+
+    const [lat, setLat] = useState(null)
+    const [lng, setLng] = useState(null)
+    const [status, setStatus] = useState(null)
+
+    function getLocation () {
+        if (!navigator.geolocation) {
+        setStatus('Geolocation not supported by your browser.');
+        } else {
+        setStatus('Locating...');
+        navigator.geolocation.getCurrentPosition((position) => {
+            setStatus(null);
+            setLat(position.coords.latitude);
+            setLng(position.coords.longitude);
+        }, () => {
+            setStatus('Unable to retrive your location.');
+        })
+        }
+    }
+
 
     return (
         <>
             <div className="home-banner">
                 <div className="temp-widget">
-                    Local temps will post here!
+                    <div className="temp-widget-icon">
+                        <i class="fa-solid fa-3x fa-temperature-half" onClick={getLocation}></i>
+                    </div>
+                    <div className="local-temp">
+
+                    </div>
                 </div>
                 <div className="events-widget">
                     Weather Events will post here!
